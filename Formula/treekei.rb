466d85b431c313ecb17fa47e048b42150741a52a
ZiHao Liu <zihao.liu.qs@gmail.com>
2026-02-25 11:20:43 +0800

    Remove 'sha256:' prefix from checksum values

--- Formula/treekei.rb
+++ Formula/treekei.rb
@@ -6,10 +6,10 @@ class Treekei < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/zihao-liu-qs/treekei/releases/download/v0.2.3/treekei_darwin_amd64.tar.gz"
-      sha256 "sha256:eb630a47546e86a5d993a2f2c8ee7c70fa276fce2eff37fd1410a23eac369e5f"
+      sha256 "eb630a47546e86a5d993a2f2c8ee7c70fa276fce2eff37fd1410a23eac369e5f"
     elsif Hardware::CPU.arm?
       url "https://github.com/zihao-liu-qs/treekei/releases/download/v0.2.3/treekei_darwin_arm64.tar.gz"
-      sha256 "sha256:03b4585d82a41b662bbad2e154381f1a3bc718dda4968fe17d94f3bd7097fdb3"
+      sha256 "03b4585d82a41b662bbad2e154381f1a3bc718dda4968fe17d94f3bd7097fdb3"
     end
   end
 
